b54de0d0da78f396d839a0f92f16e22ad1541c83
dnelson17 <dnelson17@qub.ac.uk>
2020-10-13 02:19:52 +0100

    Updated Matrix Mutiplication testing program

--- Multiprocessing 3 - Matrix Multiplication.py
+++ Multiprocessing 3 - Matrix Multiplication.py
@@ -1,36 +1,38 @@
 #This includes code that I will use in my overleaf doc to show how a matrix
 #multiplication program will scale across a number of cores from 1-8
-#and as the size of th3e matrix grows
+#and as the size of the matrix grows
 import multiprocessing
 import time
 import numpy as np
+#from matplotlib import pyplot as plt
 
-mat_size = 40320 #chosen to be 8! so the work can be divided up nicely for any number of cores
-mat1 = np.random.rand(mat_size,mat_size)
-mat2 = np.random.rand(mat_size,mat_size)
+mat_size = 840 #chosen to be 8! so the work can be divided up nicely for any number of cores from 1-8
+mat1 = np.random.randint(10, size=(mat_size,mat_size))
+mat2 = np.random.randint(10, size=(mat_size,mat_size))
 mat3 = np.zeros((mat_size,mat_size), dtype = int)
 time_list = [] #list for the total time taken for each no of cores
 
 #the start&end parameters will tell the function what row to begin and finish on
-def matrix_mult(start,end,result):
-    for i in range(start,end):
+def matrix_mult(first_row, last_row, mat_A, mat_B, mat_C):
+    for i in range(first_row, last_row):
         for j in range(mat_size):
             for k in range(mat_size):
-                result[i][j] += mat1[i][k] * mat2[k][j]
-    return result
+                mat_C[i,j] += mat_A[i][k] * mat_B[k][j]
+    print(mat_C)
+    return mat_C
 
 
 for no_cores in range(1,9):
     result = mat3
     
     start = time.perf_counter()
-
+    
     processes = []
-
+    
     if __name__ == '__main__':
         for i in range(no_cores):
-            param = [int(i* mat_size/no_cores), int((i+1)* mat_size/no_cores),result]
-            p = multiprocessing.Process(target=matrix_mult, args=param )
+            param = [int(i * mat_size / no_cores), int((i + 1) * mat_size / no_cores), mat1, mat2, result]
+            p = multiprocessing.Process(target=matrix_mult, args=param)
             p.start()
             processes.append(p)
 
@@ -44,6 +46,16 @@ for no_cores in range(1,9):
 
         time_list.append(time_taken)
 
+        #answer = np.matmul(mat1,mat2)
+
+        #comparison = result == answer
+        #equal_arrays = comparison.all()
+
+        #if equal_arrays:
+        #    print('correct')
+
 print(time_list)
 
-# !!! Put in something to check that its actually correct, like do it with numpy and check results
+
+#plt.plot([1,2,3,4,5,6,7,8],time_list)
+#plt.show()
