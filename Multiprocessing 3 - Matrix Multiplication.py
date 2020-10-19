176915c074bcf921ad7ae220b5413ed7b1569a81
dnelson17 <dnelson17@qub.ac.uk>
2020-10-20 00:37:01 +0100

    Updated testing file and added GenRuntimeGraph

--- Multiprocessing 3 - Matrix Multiplication.py
+++ Multiprocessing 3 - Matrix Multiplication.py
@@ -6,67 +6,101 @@ import time
 import numpy as np
 #from matplotlib import pyplot as plt
 
-mat_size = 840 #chosen to be 8! so the work can be divided up nicely for any number of cores from 1-8
-mat1 = np.random.randint(10, size=(mat_size,mat_size))
-mat2 = np.random.randint(10, size=(mat_size,mat_size))
-mat3 = np.zeros((mat_size,mat_size), dtype = int)
-time_list = [] #list for the total time taken for each no of cores
 
 #the start&end parameters will tell the function what row to begin and finish on
-def matrix_mult(first_row, last_row, mat_A, mat_B, mat_C):
+def matrix_mult(first_row, last_row, mat_size, mat_A, mat_B, mat_C):
     for i in range(first_row, last_row):
         for j in range(mat_size):
             for k in range(mat_size):
                 mat_C[i,j] += mat_A[i][k] * mat_B[k][j]
-    print(mat_C)
     return mat_C
 
 
-for no_cores in range(1,9):
-    result = mat3
+def check_answer(mat_A,mat_B,mat_C):
+    answer = np.matmul(mat_A,mat_B)
+    comparison = mat_C == answer
+    if comparison.all():
+        return True
+    else:
+        return False
+
+
+def gen_time_results(mat_size,max_cores,no_runs):
+    tally = 0 #Tracker of how many calculations are correct
     
-    start = time.perf_counter()
+    mat_A = np.random.randint(10, size=(mat_size,mat_size))
+    mat_B = np.random.randint(10, size=(mat_size,mat_size))
+    mat_C = np.zeros((mat_size,mat_size), dtype = int)
+    time_mat = []
+    for no_cores in range(1,max_cores+1):
+        print(f'Running on {no_cores} cores(s)')
+        time_mat.append([])
+        for _ in range(no_runs):
+                result = mat_C
+                
+                start = time.perf_counter()
+                
+                processes = []
+                
+                if __name__ == '__main__':
+                    for i in range(no_cores):
+                        param = [round(i * mat_size / no_cores), round((i + 1) * mat_size / no_cores), mat_size, mat_A, mat_B, result]
+                        p = multiprocessing.Process(target=matrix_mult, args=param)
+                        p.start()
+                        processes.append(p)
+
+                    for process in processes:
+                        process.join()
+
+                    finish = time.perf_counter()
+
+
+                    time_taken = round(finish-start,4)
+
+                    time_mat[no_cores-1].append(time_taken)
+
+                    #tally += check_answer(mat_A,mat_B,result)
+    return time_mat#, tally
+
+
+
+def gen_results_graph(time_mat):
+    time_mat = np.array(time_mat)
+    av_time_list = time_mat.mean(axis = 1)
+    print(f'Average times: {av_time_list}')
+
+    origrinal_time = av_time_list[0]
+
+    my_speedup = []
+    no_cores = []
+
+    for i in range(1,len(av_time_list)+1):
+      my_speedup.append( origrinal_time/av_time_list[i-1] )
+      no_cores.append( i )
+
+    print(f'Average speedups: {my_speedup}')
+
+    #plt.plot(no_cores,my_speedup, label = "Actual")
+    #plt.plot(no_cores,no_cores, label = "Ideal")
+    #plt.xlabel("No of cores")
+    #plt.ylabel("Speed-up")
+    #plt.legend()
+    #plt.show()
+
+
+
+def main():
+    mat_size = 840 #chosen to be 8! so the work can be divided up nicely for any number of cores from 1-8
+    max_cores = 40
+    no_runs = 20 #the code will run on each number of cores this many times
     
-    processes = []
+    time_results, no_correct = gen_time_results(mat_size,max_cores,no_runs)
+
+    no_incorrect = no_runs*max_cores - no_correct
+    print(f'No correct: {no_correct},  No incorrect: {no_incorrect}')
+
+    gen_results_graph(time_results)
+
+
+main()
     
-    if __name__ == '__main__':
-        for i in range(no_cores):
-            param = [int(i * mat_size / no_cores), int((i + 1) * mat_size / no_cores), mat1, mat2, result]
-            p = multiprocessing.Process(target=matrix_mult, args=param)
-            p.start()
-            processes.append(p)
-
-        for process in processes:
-            process.join()
-
-        finish = time.perf_counter()
-
-        time_taken = round(finish-start,4)
-        print(f'{no_cores} cores(s) - Finished in {time_taken} seconds')
-
-        time_list.append(time_taken)
-
-        #answer = np.matmul(mat1,mat2)
-
-        #comparison = result == answer
-        #equal_arrays = comparison.all()
-
-        #if equal_arrays:
-        #    print('correct')
-
-print(time_list)
-
-
-#speedup = []
-
-#for i in range(len(results)):
-#  speedup.append( origrinal_time/results[i] )
-
-#print(speedup)
-
-#plt.plot([1,2,3,4,5,6,7,8],speedup)
-#plt.xlabel("No of cores")
-#plt.ylabel("Speed-up")
-#plt.show()
-#plt.plot([1,2,3,4,5,6,7,8],time_list)
-#plt.show()
