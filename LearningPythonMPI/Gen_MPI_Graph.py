484db7db945edd3e916de56eb27078c9b18845a3
dnelson17 <dnelson17@qub.ac.uk>
2021-01-25 13:20:27 +0000

    Updating BASH file

--- LearningPythonMPI/Gen_MPI_Graph.py
+++ LearningPythonMPI/Gen_MPI_Graph.py
@@ -9,8 +9,20 @@ for i in range(len(lines)):
         times[-1] += (str(lines[i]) + ' ')
     else:
         times.append('')
+        
+times = times[1:-1]
 
-print(times)
+for time in times:
+    temp = time.split(" ")
+    temp = temp[:-1]
+    temp[0] = int(temp[0])
+    temp[1] = int(temp[1])-1
+    for j in range(2,len(temp)):
+        temp[j] = float(temp[j])
+    print(temp)
 
-for i in range(len(times)):
-    speedups.append( times[0] / times[i] )
+
+speedups = []
+
+#for i in range(len(times)):
+#    speedups.append( times[0] / times[i] )
--- LearningPythonMPI/loop.sh
+++ LearningPythonMPI/loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in 2 3 5 9; do
-	for j in 16 32 64 128 256; do
+for i in 16 32 64 128 256; do
+	for j in 2 3 5 9; do
 		echo "$i" >> results.txt
 		echo "$j" >> results.txt
 		/usr/bin/mpiexec -n $i python3 MatMul_tester.py $j $j >> results.txt
