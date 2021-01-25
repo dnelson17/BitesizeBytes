84e8f598f1f3576daa7c956148c107f62e30b124
dnelson17 <dnelson17@qub.ac.uk>
2021-01-25 15:54:39 +0000

    Update Gen_MPI_Graph.py

--- LearningPythonMPI/Gen_MPI_Graph.py
+++ LearningPythonMPI/Gen_MPI_Graph.py
@@ -9,20 +9,52 @@ for i in range(len(lines)):
         times[-1] += (str(lines[i]) + ' ')
     else:
         times.append('')
-        
-times = times[1:-1]
 
+times = times[:-1]
+
+data=[]
 for time in times:
     temp = time.split(" ")
     temp = temp[:-1]
-    temp[0] = int(temp[0])
-    temp[1] = int(temp[1])-1
+    new_time = []
+    new_time.append(int(temp[0]))
+    new_time.append(int(temp[1])-1)
+    sum = 0
     for j in range(2,len(temp)):
-        temp[j] = float(temp[j])
-    print(temp)
+        sum += float(temp[j])
+    new_time.append(sum/new_time[1])
+    data.append(new_time)
 
+final_times = [[]]
+print(data)
+print(data[0][0])
 
-speedups = []
+current_mat_size = data[0][0]
+for i in range(0,len(data)):
+    if data[i][0] == current_mat_size:
+        final_times[-1].append(data[i][2])
+    else:
+        final_times.append([])
+        current_mat_size = data[i][0]
+        final_times[-1].append(data[i][2])
 
-#for i in range(len(times)):
-#    speedups.append( times[0] / times[i] )
+print(final_times)
+
+speedup_mat = []
+
+for i in range(len(final_times)):
+  speedup_mat.append([])
+  for j in range(len(final_times[i])):
+    speedup_mat[i].append( final_times[i][0] / final_times[i][j] )
+
+no_cores = [1,2,4,8]
+
+for x in range(len(speedup_mat)):
+  plt.plot(no_cores,speedup_mat[x], label = f"Actual - {int(data[4*x][0])}")
+plt.plot(no_cores,no_cores, label = "Ideal")
+plt.xlim(1,8)
+plt.ylim(1,8)
+plt.xlabel("No of cores")
+plt.ylabel("Speed-up")
+plt.legend()
+plt.show()
