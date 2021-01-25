fe1c5d6687817f484674f34daf23f678bc575af6
dnelson17 <dnelson17@qub.ac.uk>
2021-01-25 13:35:01 +0000

    Created tester file to generate MPI results

--- LearningPythonMPI/tester.sh
+++ LearningPythonMPI/tester.sh
@@ -4,7 +4,7 @@ for i in 16 32 64 128 256; do
 	for j in 2 3 5 9; do
 		echo "$i" >> results.txt
 		echo "$j" >> results.txt
-		/usr/bin/mpiexec -n $i python3 MatMul_tester.py $j $j >> results.txt
+		/usr/bin/mpiexec -n $j python3 MatMul_tester.py $i $i >> results.txt
 		echo "" >> results.txt
 	done
 done
