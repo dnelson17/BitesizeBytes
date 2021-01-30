83ba5e150eae3a62bdf95401df776a68d637f163
dnelson17 <dnelson17@qub.ac.uk>
2021-01-30 10:08:19 +0000

    Update MatrixMultDistribution.py

--- LearningMiscellaneous/MatrixMultDistribution.py
+++ LearningMiscellaneous/MatrixMultDistribution.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def matrix_mult(mat_A, mat_B):
     mat_C = np.zeros((mat_A.shape[0],mat_B.shape[1]))
-    print(mat_A.shape[0])
-    print(mat_B.shape[1])
+    #print(mat_A.shape[0])
+    #print(mat_B.shape[1])
     for i in range(len(mat_A)):
         for j in range(len(mat_B[i])):
             for k in range(len(mat_B)):
@@ -38,7 +38,7 @@ def tester_3(size, mat_size):
     #Assuming for simplicity that no processesors, size, is a power of 2
     p = int(np.sqrt(size))
     factor = 2**(int(np.log2(size))%2)
-    print(factor)
+    #print(factor)
     send_list = []
     for i in range(factor*p):
         first_i = round(i * mat_size / (factor*p))
@@ -46,21 +46,33 @@ def tester_3(size, mat_size):
         for j in range(p):
             first_j = round(j * mat_size / p)
             last_j = round((j + 1) * mat_size / p)
-            print(mat_A[first_i:last_i,:])
-            print(mat_B[:,first_j:last_j])
+            #print(mat_A[first_i:last_i,:])
+            #print(mat_B[:,first_j:last_j])
             mat_C = matrix_mult(mat_A[first_i:last_i,:],mat_B[:,first_j:last_j])
-            print(mat_C)
+            #print(mat_C)
             print("\n")
             send_list.append([mat_A[first_i:last_i,:],mat_B[:,first_j:last_j]])
+            print(f"i: {i}")
+            print(f"j: {j}")
+            print(f"Rank: {i*2 + j}")
     return send_list
 
 
 #tester_2(4,8)
 print("\n\n")
-mat_list = tester_3(4,4)
+mat_list = tester_3(8,8)
 
-print("\n\n\n\n\n")
+#print("\n\n\n\n\n")
 
-for mat in mat_list:
-    mat_C = matrix_mult(mat[0],mat[1])
-    print(mat_C)
+#for mat in mat_list:
+#    mat_C = matrix_mult(mat[0],mat[1])
+#    print(mat_C)
+
+for i in range(10):
+    n = 2**i
+    print(f"n = 2^{i} = {2**i}")
+    factor = int(2**(int(np.log2(n))%2))
+    power = np.log2(n)/2
+    print(f"power: {power}")
+    print(np.arange(n).reshape(( int(2**np.ceil(power)) , int(2**np.floor(power)) )) )
+
