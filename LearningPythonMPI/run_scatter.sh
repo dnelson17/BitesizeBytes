a59674c9d5ae14aec4f41445edbe9664ae6fd569
dnelson17 <dnelson17@qub.ac.uk>
2021-01-31 13:54:56 +0000

    Created run_scatter.sh and scatter_results.txt
    The bash file will run over the scatter.py file and save results (timings) to scatter_results.txt so they can be created into a graph in a different file

---
+++ LearningPythonMPI/run_scatter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for mat_size in 8 16 32 128 256 512; do
+	for p in 1 2 4 8; do
+		echo "$mat_size" >> scatter_results.txt
+		echo "$p" >> scatter_results.txt
+		/usr/bin/mpiexec -n $p python3 scatter.py $mat_size $mat_size >> scatter_results.txt
+		echo "" >> scatter_results.txt
+	done
+done
