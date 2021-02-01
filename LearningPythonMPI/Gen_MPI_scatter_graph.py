814ba36046b062b8aab4226a8b686baa4c9978c1
dnelson17 <dnelson17@qub.ac.uk>
2021-02-01 10:37:44 +0000

    Update Gen_MPI_scatter_graph.py

--- LearningPythonMPI/Gen_MPI_scatter_graph.py
+++ LearningPythonMPI/Gen_MPI_scatter_graph.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
 
+#Open file
 with open('scatter_results.txt') as f:
     lines = [line.rstrip() for line in f]
 
+#Read in each line
 times = ['']
 for i in range(len(lines)):
     if lines[i] != '':
@@ -10,10 +12,7 @@ for i in range(len(lines)):
     else:
         times.append('')
 
-times = times[:-1]
-
-print(times)
-
+#Take average time for each instance
 data=[]
 for time in times:
     temp = time.split(" ")
@@ -21,14 +20,14 @@ for time in times:
     new_time = []
     new_time.append(int(temp[0]))
     new_time.append(int(temp[1]))
-    sum = 0
     for j in range(2,len(temp)):
         temp[j] = float(temp[j])
-    new_time.append(sum(temp[2:])/len(temp[2:]))
+    new_time.append((sum(temp[2:])/len(temp[2:])))
     data.append(new_time)
 
 final_times = [[]]
 
+#Group all results corresponding to matching matrix sizes
 current_mat_size = data[0][0]
 for i in range(0,len(data)):
     if data[i][0] == current_mat_size:
@@ -38,17 +37,15 @@ for i in range(0,len(data)):
         current_mat_size = data[i][0]
         final_times[-1].append(data[i][2])
 
-print(final_times)
-
 speedup_mat = []
 
+#Turn raw times into speed-ups
 for i in range(len(final_times)):
   speedup_mat.append([])
   for j in range(len(final_times[i])):
     speedup_mat[i].append( final_times[i][0] / final_times[i][j] )
 
-print(speedup_mat)
-
+#Core list for x-axis
 no_cores = [1,2,4,8]
 
 for x in range(len(speedup_mat)):
