381937e708b102308276ede945e984d6fa6cf26a
dnelson17 <dnelson17@qub.ac.uk>
2021-02-09 10:47:47 +0000

    Update MatrixMultNumpy.py

--- LearningPythonMPI/MPI_IO/MatrixMultNumpy.py
+++ LearningPythonMPI/MPI_IO/MatrixMultNumpy.py
@@ -17,8 +17,8 @@ def matrix_mult(mat_A, mat_B):
     mat_C = np.zeros((mat_A.shape[0],mat_B.shape[0]))
     print(mat_C.shape)
     for i in range(len(mat_A)):
-        for j in range(len(mat_B[i])):
-            for k in range(len(mat_B)):
+        for j in range(len(mat_B)):
+            for k in range(len(mat_B[i])):
                 mat_C[i,j] += mat_A[i][k] * mat_B[j][k]
     return mat_C
 
