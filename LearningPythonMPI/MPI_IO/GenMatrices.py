4e81214ff3450974ca1337887475b11b5f050717
dnelson17 <dnelson17@qub.ac.uk>
2021-02-09 21:21:59 +0000

    Running tests again

--- LearningPythonMPI/MPI_IO/GenMatrices.py
+++ LearningPythonMPI/MPI_IO/GenMatrices.py
@@ -3,13 +3,15 @@ import sys
 
 mat_size = 8
 # Will use this when testing such that it can be looped efficiently
-mat_size = int( sys.argv[1])
-iteration = int( sys.argv[2])
+#mat_size = int( sys.argv[1])
+#iteration = int( sys.argv[2])
 
 mat_A = np.random.rand(mat_size,mat_size)
 #As B is an arbitrary matrix, we will take the transpose of it
 mat_B = np.random.rand(mat_size,mat_size)
 mat_B = np.transpose(mat_B)
 
-np.savetxt(f"mat_A_{mat_size}_{iteration}.txt", mat_A)
-np.savetxt(f"mat_B_{mat_size}_{iteration}.txt", mat_B)
+#np.savetxt(f"mat_A_{mat_size}_{iteration}.txt", mat_A)
+#np.savetxt(f"mat_B_{mat_size}_{iteration}.txt", mat_B)
+np.savetxt(f"mat_A.txt", mat_A)
+np.savetxt(f"mat_B.txt", mat_B)
