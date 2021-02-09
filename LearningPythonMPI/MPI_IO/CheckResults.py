f7abf5f5dcd41eec153cbc0d0f8754de1abdf237
dnelson17 <dnelson17@qub.ac.uk>
2021-02-09 11:11:43 +0000

    Update CheckResults.py

--- LearningPythonMPI/MPI_IO/CheckResults.py
+++ LearningPythonMPI/MPI_IO/CheckResults.py
@@ -1,7 +1,25 @@
 import numpy as np
 
+def matrix_mult(mat_A, mat_B):
+    mat_C = np.zeros((mat_A.shape[0],mat_B.shape[0]))
+    print(mat_C.shape)
+    for i in range(len(mat_A)):
+        for j in range(len(mat_B)):
+            for k in range(len(mat_B[i])):
+                mat_C[i,j] += mat_A[i][k] * mat_B[j][k]
+    return mat_C
+
+
 mat_A = np.loadtxt("mat_A.txt")
 mat_B = np.loadtxt("mat_B.txt")
 
-mat_C = np.matmul(mat_A,mat_B)
+#answer = np.matmul(mat_A,mat_B)
+answer = matrix_mult(mat_A,mat_B)
 
+print(answer)
+
+res = np.loadtxt("mat_C.txt")
+
+print(res)
+
+print(np.array_equal(answer,res))
