843546470a167f48acdde40390555eacb9d7df0f
dnelson17 <dnelson17@qub.ac.uk>
2021-02-23 00:13:59 +0000

    Updating MPI_IO tester files

--- LearningPythonMPI/MPI_IO/run_MPI_IO.sh
+++ LearningPythonMPI/MPI_IO/run_MPI_IO.sh
@@ -5,7 +5,7 @@ for mat_size in 128 256 512 1024 2048 4096 8192 16384 32768; do
 	echo "" >> mpi_io_results.txt
 	echo "$mat_size"
 	echo ""
-	for i in 1 2 3 4 5; do
+	for i in 1 2 3 4 5 6 7 8 9 10; do
 		/usr/bin/mpiexec -n 1 python3 GenMatrices.py $mat_size $i
 		for p in 1 2 4 8; do
 			echo "$p" >> mpi_io_results.txt
