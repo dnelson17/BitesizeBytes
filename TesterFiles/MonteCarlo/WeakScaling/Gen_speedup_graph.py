fb02a21a7477ad926f7ab0b30c1ac9b8df56b7c9
dnelson17 <dnelson17@qub.ac.uk>
2021-03-23 11:48:54 +0000

    Updating MonteCarlo Files

---
+++ TesterFiles/MonteCarlo/WeakScaling/Gen_speedup_graph.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+
+speedup_df = pd.read_pickle("speedup_df_weak_scaling.pkl")
+speedup_df = speedup_df.drop([1,2,3,4])
+speedup_df = speedup_df.T
+
+speedup_df.plot()
+plt.xlabel("Number of Cores")
+plt.ylabel("Normalised Runtime")
+plt.legend()
+plt.savefig(f"montecarlo_speedup_{program_type}_scaling.jpeg", dpi=1000)
---
+++ TesterFiles/MonteCarlo/WeakScaling/MonteCarlo_Weak_Tester.py
@@ -0,0 +1,61 @@
+from multiprocessing import Pool
+import pandas as pd
+import numpy as np
+import time
+import sys
+
+def monte_carlo(attempts):
+    i = 0
+    hits = 0
+    while i < attempts:
+        dots = np.random.rand(1,2)
+        if dots[0][0]**2 + dots[0][1]**2 <= 1:
+            hits += 1
+        i += 1
+    return hits
+
+
+def gen_time_results(attempts, no_cores):
+    print(f"{no_cores} core(s)")
+    start = time.perf_counter()
+    send_list = [(attempts,) for _ in range(no_cores)]
+    p = Pool(processes=no_cores)
+    hits_list = p.starmap(monte_carlo, (send_list))
+    p.close()
+    finish = time.perf_counter()
+    total_hits = np.sum(hits_list)
+    approx_pi = 4*total_hits/(attempts*no_cores)
+    print(f"Estimation: {approx_pi}")
+    time_taken = round(finish-start,10)
+    print(f"Time: {time_taken}\n")
+    return time_taken
+
+
+def main():
+    attempts_list = [10**n for n in range(1,9)]
+    core_list = [2**i for i in range(6)]
+    time_df = pd.DataFrame(columns=core_list)
+    for attempts in attempts_list:
+        print(f"Attempts = 10^{int(np.log10(attempts))}")
+        new_times = []
+        for no_cores in core_list:
+            time_taken = gen_time_results(attempts, no_cores)
+            new_times.append(time_taken)
+        time_df = time_df.append( pd.DataFrame([new_times],columns=core_list,index=[int(np.log10(attempts))]) )
+        time_df.to_pickle("time_df_weak_scaling.pkl")
+        print("")
+    print(f"\nOriginal times:\n{time_df}")
+    time_df = time_df.sort_index()
+    time_df = time_df.groupby(time_df.index).mean()
+    print(f"\nTimes after ordering and mean:\n{time_df}")
+    speedup_df = time_df.apply(lambda x: x/x.iloc[0], axis=1, result_type='expand')
+    ideal_df = pd.DataFrame([[1 for _ in range(6)]],columns=core_list,index=["Ideal"])
+    print(f"\nIdeal:\n{ideal_df}")
+    speedup_df = speedup_df.append( ideal_df )
+    print(f"\nSpeedups:\n{speedup_df}")
+    time_df.to_pickle("time_df_weak_scaling.pkl")
+    speedup_df.to_pickle("speedup_df_weak_scaling.pkl")
+
+
+if __name__ == '__main__':
+    main()
