f8c094503e215bd61617ed16bc56fdef1704d451
dnelson17 <dnelson17@qub.ac.uk>
2021-03-24 16:36:30 +0000

    Updating MPI bash files

--- TesterFiles/MPI/Lapack/run_scatter.sh
+++ TesterFiles/MPI/Lapack/run_scatter.sh
@@ -3,17 +3,14 @@
 python Gen_empty_dfs.py
 
 for mat_size in 5 6 7 8 9 10 11 12 13; do
-	for p in 1 2 4 8 16 32; do
-			echo "$mat_size" >> scatter_results.txt
-			echo "$p" >> scatter_results.txt
-			echo "$mat_size"
+	echo "$mat_size"
+	echo ""
+	for i in 1 2 3 4 5 6 7 8 9 10; do
+		echo "$i"
+		for p in 1 2 4 8 16 32; do
 			echo "$p"
-			echo ""
-			for i in 1 2 3 4 5 6 7 8 9 10; do
-				echo "$i"
-				/usr/bin/mpiexec -n $p python3 scatter_lapack_sgemm.py $mat_size $mat_size >> scatter_results.txt
+			mpiexec -n $p python MatrixMult_SGEMM.py $mat_size
 			done
 			echo ""
-			echo "" >> scatter_results.txt
 	done
 done
