06912229e57ae31bf7c755d93832264d09385686
dnelson17 <dnelson17@qub.ac.uk>
2021-04-25 22:58:54 +0100

    Updating Fortran files

--- TesterFiles/Libraries/GenRuntimeGraph.py
+++ TesterFiles/Libraries/GenRuntimeGraph.py
@@ -1,6 +1,25 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
+def read_fortran_times():
+    fortran_df = pd.read_fwf("fortran_results.txt")
+    fortran_df.columns = ["NumProcs","MatSize","Times"]
+    fortran_df = fortran_df.pivot(index="MatSize",columns="NumProcs",values="Times")
+    return fortran_df
+
+
+def gen_cubed_df(start_power,end_power):
+    order_list = [2**j for j in range(start_power,end_power)]
+    cubed_list = [(2**j)**3 for j in range(start_power,end_power)]
+    cubed_df = pd.DataFrame([cubed_list],columns=order_list,index=["(n^3)/100"])
+    return cubed_df.T
+
+
+def apply_norm(time_df,norm_index):
+    norm_df = (time_df.T).apply(lambda x: x/x.iloc[norm_index], axis=1, result_type='expand')
+    return norm_df
+
+
 norm_factor = 5
 print(f"\n\nfactor: {norm_factor}")
 norm_power = range(5,14)[norm_factor]
