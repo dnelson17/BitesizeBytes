f8f0a970ec05ada391f15b83f4fdd7b677eb167c
dnelson17 <dnelson17@qub.ac.uk>
2021-04-28 16:21:34 +0100

    Updating MPI MyFunc graph files

--- TesterFiles/MPI/MyFunc/Gen_MPI_speedup_graph.py
+++ TesterFiles/MPI/MyFunc/Gen_MPI_speedup_graph.py
@@ -2,17 +2,20 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 import pandas as pd
 
-pkl_list = ["read", "calc", "write", "total"]
+pkl_list = ["scatter", "calc", "gather", "total"]
 core_list = [2**i for i in range(6)]
 p = Path.cwd()
 
 for pkl_name in pkl_list:
+    print(f"\n\n{pkl_name}")
     time_df = pd.read_pickle(f"Time_dfs/{pkl_name}_df.pkl")
     time_df = time_df.sort_index()
     time_df = time_df.groupby(time_df.index).mean()
+    print(time_df)
     speedup_df = time_df.apply(lambda x: x.iloc[0]/x, axis=1, result_type='expand')
     ideal_df = pd.DataFrame([core_list],columns=core_list,index=["Ideal"])
     speedup_df = speedup_df.append( ideal_df )
+    print(speedup_df)
     speedup_df = speedup_df.T
     speedup_df.plot()
     plt.xlabel("Number of Cores")
