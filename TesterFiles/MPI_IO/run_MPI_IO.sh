76f415e59b6480a293b061b035f1d783fc88c207
dnelson17 <72336679+dnelson17@users.noreply.github.com>
2021-05-13 22:16:05 +0100

    Update run_MPI_IO.sh

--- TesterFiles/MPI_IO/run_MPI_IO.sh
+++ TesterFiles/MPI_IO/run_MPI_IO.sh
@@ -2,10 +2,9 @@
 
 python Gen_empty_dfs.py
 
-for mat_size in 10 11 12 13 14 15; do
-	echo "mat: 2^$mat_size"
-	echo ""
+for mat_size in 10 11 12 13 14 15 16; do
 	for i in 1 2 3 4 5 6 7 8 9 10; do
+		echo "mat: 2^$mat_size"
 		echo "i: $i"
 		mpiexec -n 1 python GenMatrices.py $mat_size $i
 		for p in 1 2 4 8 16 32; do
